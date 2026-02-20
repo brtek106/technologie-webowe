d9d87cd689286b069ced334efff51284503384a5
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2026-02-20 17:37:58 +0100

    JavaScript - definiowanie skryptów

---
+++ JavaScript/skrypt1.js
@@ -0,0 +1,9 @@
+const day = 123;
+            const hours = day * 24;
+            const minutes = hours * 60;
+            const seconds = minutes * 60;
+
+            const textToDisplay = "Od początku roku minęło " + day + "dni. To " + hours + " godzin, " 
+            + minutes + " minut lub " + seconds + " sekund."
+
+            alert(textToDisplay);
