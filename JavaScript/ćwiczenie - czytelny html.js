7e5a38076830a3bc70779187fca351a935d7c268
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2026-02-27 12:20:19 +0100

    JavaScript - ćw. czytelny HTML

---
+++ JavaScript/ćwiczenie - czytelny html.js
@@ -0,0 +1,21 @@
+const title = "Kurs programowania";
+const h1 = "Kurs programowania w JavaScript";
+const p = "Ucz się na praktycznych przykładach, rozwiązując dziesiątki zadań.";
+const price = 299;
+let html = "<html>";
+
+let str = `
+${html}
+    <head>
+        <title>${title}</title>
+    </head>
+    <body>
+        <h1>${h1}</h1>
+        <p>${p}</p>
+        <p>Cena kursu: ${price}</p>
+    </body>
+</html>
+`;
+
+
+console.log(str);
