14e13cf67581d1d6d63ce59900a0596f2d27280d
Bartek <bartlomiej.dudek94@gmail.com>
2026-03-31 16:06:41 +0200

    Serwlety - obiekt odpowiedzi

--- Jakarta/params/src/main/java/pl/javastart/params/HelloController.java
+++ Jakarta/params/src/main/java/pl/javastart/params/HelloController.java
@@ -1,19 +1,22 @@
 package pl.javastart.params;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/hello")
 public class HelloController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
-        System.out.println("Cześć " + firstName + " " + lastName);
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        PrintWriter writer = response.getWriter();
+        writer.println("Cześć " + firstName + " " + lastName);
     }
 }
