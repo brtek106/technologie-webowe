658da9db28e76cc20d08b4018de330f86e14a0d7
Bartek <bartlomiej.dudek94@gmail.com>
2026-04-13 10:22:16 +0200

    Formularze i metoda POST - ćw. walidator haseł

---
+++ JSP/password/src/main/java/pl/bartek/password/Password.java
@@ -0,0 +1,16 @@
+package pl.bartek.password;
+
+public class Password {
+    private String password;
+
+    Password() {
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
