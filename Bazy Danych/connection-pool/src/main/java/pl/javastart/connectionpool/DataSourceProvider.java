b452cb0f374b040b2acc4c38c9707c83e682c99b
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2026-06-11 10:56:36 +0200

    Bazy danych w aplikacjach webowych - pule połączeń

---
+++ Bazy Danych/connection-pool/src/main/java/pl/javastart/connectionpool/DataSourceProvider.java
@@ -0,0 +1,23 @@
+package pl.javastart.connectionpool;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+class DataSourceProvider {
+    private static DataSource dataSource;
+
+    private DataSourceProvider() {
+
+    }
+
+    static DataSource getDataSource() throws NamingException {
+        if (dataSource == null) {
+            InitialContext initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:comp/env/");
+            dataSource = (DataSource) envContext.lookup("jdbc/cookbook");
+        }
+        return dataSource;
+    }
+}
