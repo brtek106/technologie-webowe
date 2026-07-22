e69c193d67ea2c39d2b9c37da5a7f340d95e8655
Bartek Dudek <bartlomiej.dudek94@gmail.com>
2026-07-22 07:46:58 +0200

    Projekt - Implementacja rejestracji

---
+++ Projekt ReadStack/readstack/src/main/java/pl/javastart/readstack/client/signup/SignupController.java
@@ -0,0 +1,35 @@
+package pl.javastart.readstack.client.signup;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import pl.javastart.readstack.domain.api.UserRegistration;
+import pl.javastart.readstack.domain.api.UserService;
+
+import java.io.IOException;
+
+@WebServlet("/signup")
+public class SignupController extends HttpServlet {
+    private final UserService userService = new UserService();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("WEB-INF/views/signup.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserRegistration userRegistration = getUserData(req);
+        userService.register(userRegistration);
+        resp.sendRedirect(req.getContextPath());
+    }
+
+    private UserRegistration getUserData(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        return new UserRegistration(username, email, password);
+    }
+}
